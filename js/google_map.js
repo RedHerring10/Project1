16078bcfe1c441462b88f0fb8abf3ee11a43b533
RedHerring10 <BernardAsongu@gmail.com>
2025-04-14 20:43:00 -0400

    3rd attempt at adding map

--- js/google_map.js
+++ js/google_map.js
@@ -1,5 +1,14 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    await customElements.whenDefined('gmpx-store-locator');
-    const locator = document.querySelector('gmpx-store-locator');
-    locator.configureFromQuickBuilder(CONFIGURATION);
-  });
+function initMap() {
+    const mapCenter = { lat: 43.001795378459, lng: -78.78949171731864 };
+    
+    const map = new google.maps.Map(document.getElementById("map"), {
+        center: mapCenter,
+        zoom: 14,
+    });
+
+    const marker = new google.maps.Marker({
+        position: mapCenter,
+        map: map,
+        title: "Old U.S. Nearu Location",
+    });
+}
