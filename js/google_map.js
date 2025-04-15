050c5ad7d13a96b6fa85732e2741c0ebef486230
RedHerring10 <BernardAsongu@gmail.com>
2025-04-15 02:29:21 -0400

    Added sliding pictures

--- js/google_map.js
+++ js/google_map.js
@@ -2,3 +2,21 @@
 document.getElementById("back-to-top").onclick = function() {
   window.scrollTo({ top: 0, behavior: 'smooth' })
 }
+
+let currentIndex = 0;
+
+function changeSlide(direction) {
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+
+    currentIndex += direction;
+
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    } else if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+
+    const slider = document.querySelector('.slider');
+    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
