54c4847d2bacbe739dba7e4d03c314c9efb1c171
RedHerring10 <BernardAsongu@gmail.com>
2025-04-15 03:09:10 -0400

    Update google_map.js

--- js/google_map.js
+++ js/google_map.js
@@ -3,27 +3,6 @@ document.getElementById("back-to-top").onclick = function() {
   window.scrollTo({ top: 0, behavior: 'smooth' })
 }
 
-let currentIndex = 0;
-
-function changeSlide(direction) {
-    const slides = document.querySelectorAll('.slide');
-    const totalSlides = slides.length;
-
-    currentIndex += direction;
-
-    if (currentIndex < 0) {
-        currentIndex = totalSlides - 1;
-    } else if (currentIndex >= totalSlides) {
-        currentIndex = 0;
-    }
-
-    const slider = document.querySelector('.slider');
-    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
-
-const slides = document.querySelectorAll(".slides img");
-let slideIndex = 0;
-let intervalId = null;
 
 initializeSlider();
 
