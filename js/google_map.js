ec8b16cd79a39c7763cc68d17b5461f8fb58d5cd
RedHerring10 <BernardAsongu@gmail.com>
2025-04-15 01:36:27 -0400

    Scroll to top button test

--- js/google_map.js
+++ js/google_map.js
@@ -1,14 +1,4 @@
-function initMap() {
-    const mapCenter = { lat: 43.001795378459, lng: -78.78949171731864 };
-    
-    const map = new google.maps.Map(document.getElementById("map"), {
-        center: mapCenter,
-        zoom: 14,
-    });
-
-    const marker = new google.maps.Marker({
-        position: mapCenter,
-        map: map,
-        title: "Old U.S. Nearu Location",
-    });
+// Scroll back to top when the button is clicked
+document.getElementById("back-to-top").onclick = function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' })
 }
