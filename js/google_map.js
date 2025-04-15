baecf03d10a2ff2d46fb6a328f16c1fbb97028e6
RedHerring10 <BernardAsongu@gmail.com>
2025-04-15 03:01:27 -0400

    Tried to fix slider

--- js/google_map.js
+++ js/google_map.js
@@ -20,3 +20,40 @@ function changeSlide(direction) {
     const slider = document.querySelector('.slider');
     slider.style.transform = `translateX(-${currentIndex * 100}%)`;
 }
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+initializeSlider();
+
+function initializeSlider(){
+    slides[slideIndex].classList.add("displaySlider");
+    intervalId = setInterval(nextSLider, 5000);
+}
+
+function showSlider(index){
+
+    if(index >= slides.length){
+        slideIndex = 0;
+    }
+    else if(index < 0){
+        slideIndex = slides.length - 1;
+    }
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlider(){
+    slideIndex--;
+    showSlider(slideIndex)
+
+}
+
+function nextSLider(){
+    slideIndex++;
+    showSlider(slideIndex);
+}
